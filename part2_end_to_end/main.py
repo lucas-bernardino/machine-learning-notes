50fdb5f9db39372399fc1524f622f1e6c4130ecd
lucas-bernardino <lucasgber@gmail.com>
2024-07-22 14:27:55 -0300

    Add visualizing_geographical_data and correlations functions

--- part2_end_to_end/main.py
+++ part2_end_to_end/main.py
@@ -26,6 +26,7 @@ def get_biased_strat_sampling():
     # Now, we can do a stratified sampling based on the income category by using the StratifiedShuffleSplit class from sklearn.
     from sklearn.model_selection import StratifiedShuffleSplit
     split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    strat_train_set = strat_test_set = df.loc[0]
     for train_index, test_index in split.split(df, df["income_cat"]):
         strat_train_set = df.loc[train_index]
         strat_test_set = df.loc[test_index]
@@ -34,3 +35,59 @@ def get_biased_strat_sampling():
     for set_ in (strat_train_set, strat_test_set):
         set_.drop("income_cat", axis=1, inplace=True)
 
+    return strat_train_set, strat_test_set
+    
+def visualizing_geographical_data():
+    df_train_copy, _ = get_biased_strat_sampling()
+    
+    # We can look at house pricings with the following code.
+    # The radius of each circle represents the district's population (option s), and the color represents the price (option c)
+    # The color map is obtained from the option cmap called jet, which ranges from blue - low values - to red - high prices.
+    df_train_copy.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4, s=df_train_copy["population"]/100, 
+                       label="population", c=df_train_copy["median_house_value"], cmap=plt.get_cmap("jet"), colorbar=True)
+    plt.legend()
+    plt.show()
+
+def getting_correlations():
+    df_train, _ = get_biased_strat_sampling()
+    df_train_copy = df_train.drop(columns='ocean_proximity') # Since it's made up of words and not numbers
+    
+    # We can look at hou much each attribute correleates with the median house value. It ranges from -1 to 1. 
+    # When it's close to 1, it means that there is a strong positive correlation.
+    # When it's close to -1, it means there is a strong negative correlation.
+    # When it's close to zero, it means that there's no linear correlation.
+    corr_matrix = df_train_copy.corr()
+    print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+    # We can also use scatter_matrix function from pandas to visualize every numerical attribute against
+    # every other numerical attribute.
+    
+    # from pandas.plotting import scatter_matrix
+    # df_columns_list = df_train_copy.columns.values
+    # scatter_matrix(df_train_copy[df_columns_list])
+    # plt.show()
+
+    # The most promising attribute to predict the median house value is the median income. Let's plot their correlation
+    # We can see that there's a horizontal line around $450,000 and another around $350,000.
+    # Removing the corresponding districts would prevent the algorithm from learning to reproduce these data quirks.
+    df_train_copy.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
+    plt.show()
+
+    
+    # We can also play around with our dataset and get more mathematical information.
+    # For instance, the total number of rooms in a distrct is novery useful if we don't know how many households are there.
+    # What we really want is the number of rooms per household. Similarly, the total number of bedrooms by itself is not 
+    # very useful: we probably want to compare it to the number of rooms. And the population per household also semms
+    # like an interesting attribute combination to look at. We can create these attributes:
+    df_train_copy["rooms_per_household"] = df_train_copy["total_rooms"] / df_train_copy["households"]
+    df_train_copy["bedrooms_per_room"] = df_train_copy["total_bedrooms"] / df_train_copy["total_rooms"]
+    df_train_copy["population_per_household"] = df_train_copy["population"] / df_train_copy["households"]
+
+    corr_matrix = df_train_copy.corr()
+    print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+    # We can see that our new attributes are much more correlated with the median house than the ones we had before.
+
+
+if __name__ == "__main__":
+    getting_correlations()
